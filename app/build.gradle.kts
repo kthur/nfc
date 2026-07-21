9050dbd9558a07ddb37761f1f8b599bc160c2523
KYUNGTAK HUR <kyungtak.hur@gmail.com>
2026-07-22 01:48:49 +0900

    Clean Gradle DSL build files and remove invalid extension block causing build errors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("com.google.firebase.appdistribution")
 }
 
 android {
@@ -28,18 +27,6 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-            firebaseAppDistribution {
-                appId = System.getenv("FIREBASE_APP_ID") ?: ""
-                groups = "testers"
-                artifactType = "APK"
-            }
-        }
-        debug {
-            firebaseAppDistribution {
-                appId = System.getenv("FIREBASE_APP_ID") ?: ""
-                groups = "testers"
-                artifactType = "APK"
-            }
         }
     }
     compileOptions {
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,6 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.2.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-        classpath("com.google.firebase:firebase-appdistribution-gradle:5.0.0")
-    }
+plugins {
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
 
 tasks.register("clean", Delete::class) {
